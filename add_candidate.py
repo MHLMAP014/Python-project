dede28dfebdc45d6617416085a782e3dba477bf5
Flexiblebitch <167990603+Flexiblebitch@users.noreply.github.com>
2024-04-24 14:41:46 +0200

    Add files via upload

---
+++ add_candidate.py
@@ -0,0 +1,7 @@
+# Election platform
+# By Group 5
+print(" Welcome to the election platform\n You must be 18 years or older to parttake in the elections\n ")
+print(" Please note that you must be a South African citizen to participate in the election")
+
+candidate_name= input(" Please enter your name and surname: ")
+DOB= float(input(" Please enter your date of birth in the form yymmdd"))
